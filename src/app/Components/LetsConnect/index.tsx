439514822627a621eb6985b7c6eb5e6aca2c7b93
KeplerFall <kepler@myeshop.com.br>
2024-05-13 11:36:32 -0300

    changed the lets connect

--- src/app/Components/LetsConnect/index.tsx
+++ src/app/Components/LetsConnect/index.tsx
@@ -5,6 +5,8 @@ function LetsConnect(){
         <div className="p-4 mb-12 xl:max-w-[1280px] xl:m-auto" id="contact">
             <h2 className="font-bebas text-[44px] text-white mb-3">LET&apos;S CONNECT</h2>
             <p className="text-[#C7C7C7] font-manrope">Say hello at <a className="underline decoration-[#D3E97A] text-white" href="mailto: keplermpereira@gmail.com">keplermpereira@gmail.com</a></p>
+            <p className="text-[#C7C7C7] font-manrope">or, you can contact me directly via my <a className="underline decoration-[#D3E97A] text-white" href="https://www.linkedin.com/in/keplerpereira/">Linkedin</a> page.</p>
+        
         </div>
     )
 }
