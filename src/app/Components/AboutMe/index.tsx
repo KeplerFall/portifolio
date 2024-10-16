45380c5730a68641a81fe2ef23cf9890f4e072c9
KeplerFall <=>
2024-10-16 15:21:08 -0300

    changed about-me

--- src/app/Components/AboutMe/index.tsx
+++ src/app/Components/AboutMe/index.tsx
@@ -2,7 +2,9 @@ function AboutMe(){
     return(
         <section className="p-4 xl:max-w-[1280px] xl:m-auto">
             <h2 className="font-bebas text-[44px] text-white mb-3">ABOUT ME</h2>
-            <p>Born and raised in Ribeirão Preto, a city in the interior of the state of São Paulo, Brazil. I have been studying the technology area since 2014, where I started a technical IT course, where I learned about the concepts of most stacks, from front-end to hardware maintenance. I studied computer science at Unip College for 2 years, and I am currently studying analysis and development at the public college FATEC. When I have some time I have some hobbies, such as playing the guitar, I like playing strategy and competitive games, I also dabble in the kitchen, and I dare to say that I am the best cook in my house.</p>
+            <p>Born and raised in Ribeirão Preto, a city in the interior of São Paulo, Brazil, I've been passionate about technology since 2014 when I began a technical IT course. This early exposure gave me a solid foundation in various stacks, ranging from front-end development to hardware maintenance. I studied Computer Science at Unip for two years, and I’m currently pursuing a degree in Systems Analysis and Development at FATEC, a public institution.
+            <br />
+            In my spare time, I enjoy playing guitar, engaging in strategy and competitive games, and experimenting in the kitchen—where I proudly hold the title of the best cook in my household.</p>
         </section>
     )
 }
