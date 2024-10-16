96c3e789c2fc36962350fecf9c8c513fa51059f1
KeplerFall <=>
2024-10-16 15:23:50 -0300

    fix: about-me aptos

--- src/app/Components/AboutMe/index.tsx
+++ src/app/Components/AboutMe/index.tsx
@@ -2,7 +2,7 @@ function AboutMe(){
     return(
         <section className="p-4 xl:max-w-[1280px] xl:m-auto">
             <h2 className="font-bebas text-[44px] text-white mb-3">ABOUT ME</h2>
-            <p>Born and raised in Ribeirão Preto, a city in the interior of São Paulo, Brazil, I've been passionate about technology since 2014 when I began a technical IT course. This early exposure gave me a solid foundation in various stacks, ranging from front-end development to hardware maintenance. I studied Computer Science at Unip for two years, and I’m currently pursuing a degree in Systems Analysis and Development at FATEC, a public institution.
+            <p>Born and raised in Ribeirão Preto, a city in the interior of São Paulo, Brazil, I've been passionate about technology since 2014 when I began a technical IT course. This early exposure gave me a solid foundation in various stacks, ranging from front-end development to hardware maintenance. I studied Computer Science at Unip for two years, and I&apos;m currently pursuing a degree in Systems Analysis and Development at FATEC, a public institution.
             <br />
             In my spare time, I enjoy playing guitar, engaging in strategy and competitive games, and experimenting in the kitchen—where I proudly hold the title of the best cook in my household.</p>
         </section>
